dd110dc0850d137caf51cde5f4758a8f487ab349
Nur Mohammad <wimidnk@gmail.com>
2020-07-24 21:29:06 +0600

    first commit

---
+++ application/views/404.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Not Found</title>
+</head>
+<body>
+	<h1>Hello, You requested a page which is not created.</h1>
+</body>
+</html>
---
+++ application/views/templates/headers.php
@@ -0,0 +1,5 @@
+<html>
+	<head>
+		<title>Local Blog</title>
+	</head>
+	<body>
